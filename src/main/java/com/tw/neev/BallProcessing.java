b4387d67d2c889b6a7f4a4b7f3a5d2d3c899e3b3
“Ishwarya-Selvaraj” <ishwarya.selvaraj@thoughtworks.com>
2021-06-13 23:01:31 +0530

    [Ishu] Refactor. Change access modifier of variables

--- src/main/java/com/tw/neev/BallProcessing.java
+++ src/main/java/com/tw/neev/BallProcessing.java
@@ -5,11 +5,10 @@ import processing.core.PApplet;
 import java.util.ArrayList;
 
 public class BallProcessing extends PApplet {
-
-    public static final int BALL_COUNT = 4;
-    public static final int WIDTH = 600;
-    public static final int HEIGHT = 600;
-    public static final int FRAME = 5;
+    private static final int BALL_COUNT = 4;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 600;
+    private static final int FRAME = 5;
     ArrayList<Ball> balls = new ArrayList<>();
 
     public static void main(String[] args) {
