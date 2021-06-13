9c51f05fb92ebd6199e5e32e62aa24b9387ddab5
“Ishwarya-Selvaraj” <ishwarya.selvaraj@thoughtworks.com>
2021-06-13 22:56:43 +0530

    [Ishu] Refactor. Variable name in ball Processing

--- src/main/java/com/tw/neev/BallProcessing.java
+++ src/main/java/com/tw/neev/BallProcessing.java
@@ -10,7 +10,7 @@ public class BallProcessing extends PApplet {
     public static final int WIDTH = 600;
     public static final int HEIGHT = 600;
     public static final int FRAME = 5;
-    ArrayList<Ball> Balls = new ArrayList<>();
+    ArrayList<Ball> balls = new ArrayList<>();
 
     public static void main(String[] args) {
         PApplet.main("com.tw.neev.BallProcessing");
@@ -25,13 +25,13 @@ public class BallProcessing extends PApplet {
     @Override
     public void setup() {
         for (int ballNumber = 1; ballNumber <= BALL_COUNT; ballNumber++) {
-            Balls.add(new Ball(ballNumber, height, FRAME));
+            balls.add(new Ball(ballNumber, height, FRAME));
         }
     }
 
     @Override
     public void draw() {
-        for (Ball ball : Balls) {
+        for (Ball ball : balls) {
             ball.movePosition(this);
         }
     }
